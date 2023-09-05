ecdfe791c290510a61951884a3eede2f0a80a629
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-09-06 00:03:48 +0300

    updated leafleft maps

--- libs/dashboard/src/lib/pages/maps/leaflet/leaflet.component.ts
+++ libs/dashboard/src/lib/pages/maps/leaflet/leaflet.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import * as L from 'leaflet';
+import { Icon } from 'leaflet';
 
 @Component({
   selector: 'e-proc-leaflet',
   styleUrls: ['./leaflet.component.scss'],
   template: `
     <nb-card>
+    https://leafletjs.com/examples/custom-icons/
       <nb-card-header>Leaflet Maps</nb-card-header>
       <nb-card-body>
         <div leaflet [leafletOptions]="options"></div>
@@ -17,6 +19,11 @@ import * as L from 'leaflet';
 export class LeafletComponent {
 
   options = {
+    iconSize:     [38, 95],
+    shadowSize:   [50, 64],
+    iconAnchor:   [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor:  [-3, -76],
     layers: [
       L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
     ],
